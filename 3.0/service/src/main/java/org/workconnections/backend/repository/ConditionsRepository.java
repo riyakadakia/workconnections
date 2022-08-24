75a209451559528b89f2c58b8f9df81a64284ac1
Riya Kadakia <riyakadakia@gmail.com>
2022-08-24 05:45:11 -0700

    Missed checking in files in the last checkin

---
+++ 3.0/service/src/main/java/org/workconnections/backend/repository/ConditionsRepository.java
@@ -0,0 +1,20 @@
+package org.workconnections.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.workconnections.backend.entity.Condition;
+
+@Repository
+public interface ConditionsRepository extends MongoRepository<Condition, String> {
+	
+	public List<Condition> findAll();
+	@SuppressWarnings("unchecked")
+	public Condition save(@RequestParam("condition") Condition condition);
+	public Condition findById(@RequestParam("conditionId") int conditionId);
+	public boolean existsById(@RequestParam("conditionId") int conditionId);
+	public void deleteById(@RequestParam("conditionId") int conditionId);
+	
+}
