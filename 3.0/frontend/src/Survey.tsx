74b2150a2fe087aecc5bd0aa092cd2b4a321c3b3
Stephen Pittman <stephen@gomerits.com>
2022-09-14 18:42:04 +1000

    Add first draft for question wizard framework

--- 3.0/frontend/src/Survey.tsx
+++ 3.0/frontend/src/Survey.tsx
@@ -1,5 +1,84 @@
+import { Row, Spin } from "antd";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { surveyClient } from "./client/surveyClient";
+import { PreviousQuestion } from "./components/PreviousQuestion";
+import { QuestionCard } from "./components/QuestionCard";
 import "./Survey.css";
+import { Question, QuestionAndAnswer } from "./types";
+import { None } from "./utils/None";
+import { Some } from "./utils/Some";
 
 export function Survey() {
-  return <h2>Survey goes here</h2>;
+  // Let's ensure we have a valid sessionId as a query string parameter, and if not, probably just go to the home page
+  const navigate = useNavigate();
+  const sessionId = new URLSearchParams(window.location.search).get("sessionId");
+
+  if (None(sessionId) || sessionId === "") {
+    navigate("/");
+  }
+
+  // I'm not sure how we're going to store the dynamic structure of previous questions & answers o.O
+  // This is just a first guess
+  const [previousQuestions, setPreviousQuestions] = useState<QuestionAndAnswer[]>([]);
+
+  // This state will be used to hold the currently displayed question
+  // I wasn't expecting this to be a dynamic one passed from the server, we'll have to figure out how to tell what validation
+  // to use for each question, and how to render the input type
+  const [currentQuestion, setCurrentQuestion] = useState<Question>();
+
+  // We want to get the next question to display, and synchronize it to the previousQuestions.length state
+  // That way, if we add (or remove) questions from previousQuestions array, we will call this effect again that
+  // should make sure we're always displaying the right question on screen
+  useEffect(() => {
+    const getNextQuestion = async () => {
+      // Depending on how many questions are in the previousQuestions we need to behave differently
+      if (previousQuestions.length === 0) {
+        const question = await surveyClient.getFirstQuestion();
+        setCurrentQuestion(question);
+      }
+
+      // TODO: implement what happens when previousQuestions.length > 0
+      // It might need to call a differeny surveyClient function, passing in some parameters
+      // It must still update currentQuestion
+
+      // TODO: how do we determine if we've finished the current questions, and what happens then?
+    };
+
+    getNextQuestion();
+  }, [previousQuestions.length]);
+
+  return (
+    <>
+      {/* This will be the "Previous questions" bit from the design */}
+      {previousQuestions.map((q) => (
+        <PreviousQuestion key={q.question.id} questionAndAnswer={q} />
+      ))}
+
+      {/* Here, we will display the currentQuestion (if any loaded up yet) */}
+      {Some(currentQuestion) ? (
+        <QuestionCard
+          question={currentQuestion}
+          onNext={(answer) => {
+            // Store the previous question away with it's answer
+            setPreviousQuestions((prev) => [
+              ...prev,
+              {
+                question: currentQuestion,
+                answer,
+              },
+            ]);
+
+            // And clear the current question
+            // NOTE: we'll get a new one from the API automatically thanks to the useEffect watching the length of previousQuestions array
+            setCurrentQuestion(undefined);
+          }}
+        />
+      ) : (
+        <Row justify="center" align="middle" className="mt1 mb1">
+          <Spin size="large" />
+        </Row>
+      )}
+    </>
+  );
 }
