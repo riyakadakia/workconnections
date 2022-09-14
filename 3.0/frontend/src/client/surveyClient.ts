a238eb511752a81cbddb354c42a6f9dcb92db5df
Stephen Pittman <stephen@gomerits.com>
2022-09-14 14:06:36 +1000

    Fail if API doesn't return 2xx

--- 3.0/frontend/src/client/surveyClient.ts
+++ 3.0/frontend/src/client/surveyClient.ts
@@ -4,6 +4,14 @@ import { config } from "../config";
 const makeSurveyClient = () => {
   const httpClient = new Axios({
     baseURL: config.serverUrl,
+    validateStatus: (status) => status >= 200 && status < 300,
+  });
+
+  // Fail if API doesn't return 2xx
+  httpClient.interceptors.response.use(undefined, (error) => {
+    window.alert("Something has gone terribly wrong :(\n\n" + error);
+
+    throw error;
   });
 
   const getTotalProgramsCount = async () => {
